56218d877ccefb0951fc3edf6364b536f5e92e85
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-11-07 13:22:13 +0200

    complete

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,45 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  public tenants: Person[] = [];
+  constructor(protected key: Key) {}
+
+  public abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
-
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
-
 export {};
